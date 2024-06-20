3718715cfd700d628fdba1c7ba4dcf493beb5a96
24HP0317 24HP0317 <24HP0317@Administrators-iMac.local>
2024-06-20 16:49:47 +0800

    test

--- src/graphql/schemas/index.ts
+++ src/graphql/schemas/index.ts
@@ -1,5 +1,4 @@
 import mergeTypeDefs from "graphql-tools-merge-typedefs";
 import { teamType } from "./teamSchema";
 import { todoType } from "./todoSchema";
-import { userType } from "./userSchema";
-export const typeDefs = mergeTypeDefs([todoType, teamType, userType]);
+export const typeDefs = mergeTypeDefs([todoType, teamType]);
